f7fafecb398e3d5edf53f961ce7e9f12a67ab7d1
River <astrofinity@gmail.com>
2018-09-22 20:48:17 -0700

    Added input validation for the component

--- JavaScript/smartcar/src/components/explorer.jsx
+++ JavaScript/smartcar/src/components/explorer.jsx
@@ -1,12 +1,89 @@
 import React, { Component } from "react";
 import FieldList from "./fieldList";
 
+//this component receives a title, url, method, and body (any of which may be emp)
 class Explorer extends Component {
 	state = {
 		response: ""
 	};
 
 	render() {
+		return this.validateProps() === true ? ( //if input validation passes
+			this.printExplorer() //print the component
+		) : (
+			//otherwise print the source of the error (or)
+			<h1>Error initializing explorer component props</h1>
+		);
+	}
+
+	//check the props passed as input to see if they are of valid types and values, returns true if everything checks out
+	validateProps() {
+		if (
+			!this.props.method ||
+			typeof this.props.method != "string" ||
+			(this.props.method !== "GET" &&
+				this.props.method !== "POST" &&
+				this.props.method !== "PUT" &&
+				this.props.method !== "DELETE")
+		)
+			return false;
+
+		if (!this.props.url || typeof this.props.url != "string") return false;
+
+		if (
+			(this.props.method === "PUT" || this.props.method === "POST") &&
+			(!this.props.body || typeof this.props.body != "object")
+		) {
+			return false;
+		}
+
+		return true;
+	}
+
+	//handle the submission of the request (when the user clicks the send request button)
+	handleSubmit = event => {
+		event.preventDefault();
+		const data = new FormData(event.target);
+		//handle GET request and receieve response
+		if (this.props.url) {
+			if (this.props.method === "GET") {
+				fetch(this.props.url)
+					.then(response => response.json())
+					.then(json =>
+						this.setState({ response: JSON.stringify(json, null, 2) })
+					);
+			}
+			//handle request that requires input (and DELETE since there is no harm sending an empty body)
+			else if (this.props.body || this.props.method === "DELETE") {
+				//only process the submission if it has a body for input or the method is DELETE
+				//format the input data for sending
+				let formatData = {};
+				data.forEach(function(value, key) {
+					formatData[key] = value;
+				});
+				//send request and receive response
+				fetch(this.props.url, {
+					method: this.props.method,
+					body: JSON.stringify(formatData),
+					headers: {
+						"Content-type": "application/json; charset=UTF-8"
+					}
+				})
+					.then(response => response.json())
+					.then(json =>
+						this.setState({ response: JSON.stringify(json, null, 2) })
+					);
+			} else {
+				//no body for input detected when it was required
+				this.setState({ response: "Error, no body for input detected" });
+			}
+		} else {
+			//no url was input
+			this.setState({ response: "Error, no URL detected" });
+		}
+	};
+
+	printExplorer() {
 		return (
 			<div style={{ paddingLeft: 25 }}>
 				{this.printHeader()}
@@ -15,40 +92,6 @@ class Explorer extends Component {
 			</div>
 		);
 	}
-
-	//handle the submission of the request (when the user clicks the send request button)
-	handleSubmit = event => {
-		event.preventDefault();
-		const data = new FormData(event.target);
-		//handle GET request and receieve response
-		if (this.props.method === "GET") {
-			fetch(this.props.url)
-				.then(response => response.json())
-				.then(json =>
-					this.setState({ response: JSON.stringify(json, null, 2) })
-				);
-		}
-		//handle request that requires input (and DELETE since there is no harm sending an empty body)
-		else {
-			//format the input data for sending
-			let formatData = {};
-			data.forEach(function(value, key) {
-				formatData[key] = value;
-			});
-			//send request and receive response
-			fetch(this.props.url, {
-				method: this.props.method,
-				body: JSON.stringify(formatData),
-				headers: {
-					"Content-type": "application/json; charset=UTF-8"
-				}
-			})
-				.then(response => response.json())
-				.then(json =>
-					this.setState({ response: JSON.stringify(json, null, 2) })
-				);
-		}
-	};
 
 	//print the header for the explorer
 	printHeader() {
